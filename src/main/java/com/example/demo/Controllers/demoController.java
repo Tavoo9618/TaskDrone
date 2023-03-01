170089f31e4672e1d54932f34d13c396635bbcae
Gustavo Mackey <tavoo9618@gmail.com>
2023-03-01 15:14:27 -0500

    add features it isn't done yet

--- src/main/java/com/example/demo/Controllers/demoController.java
+++ src/main/java/com/example/demo/Controllers/demoController.java
@@ -5,6 +5,10 @@
  */
 package com.example.demo.Controllers;
 
+import com.example.demo.Drones.Medication;
+import com.example.demo.Drones.drone;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +24,37 @@ public class demoController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
+    
+    
+     public void RegisteringaDrone ( drone d){
+    
+    }
+    
+    
+    public void loadDoWMedItem(){
+        double weightlimit = 500;
+        
+    
+    }
+    
+    public List<Medication> chekingLoadMed(drone d){
+    
+        List<Medication> M = new ArrayList<>();
+        return M;
+        
+    }
+    
+    
+    public List<drone> chekavaliDronForLoad( List<drone> dro){
+    
+        List<drone> d= new ArrayList<>();
+        return d;
+        
+    }
+    
+    public String  shekDroneBatery(drone d ){
+        String B="";
+        return B;
+        }
     
 }
